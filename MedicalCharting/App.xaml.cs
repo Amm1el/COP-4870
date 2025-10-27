80afe180da3a2deaf63a680a7c0c13bf5fade9e7
Ammiel Bowen <ab22dv@fsu.edu>
2025-10-27 12:55:54 -0400

    need to edit more

--- MedicalCharting/App.xaml.cs
+++ MedicalCharting/App.xaml.cs
@@ -1,10 +1,12 @@
-namespace MedicalChartingMaui;
+using Microsoft.Maui.Controls;
+
+namespace MedicalCharting;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
-        MainPage = new AppShell();
+        MainPage = new AppShell(); // Entry point (defined in AppShell.xaml)
     }
 }
